6832d43217bf7ee5d9b8c480eb06b6ed8ceefe9b
ptndoss@gmail.com <ptndoss@gmail.com>
2020-05-20 15:17:29 -0700

    Kafka Initial Setup

---
+++ src/main/java/com/github/ptndoss/kafkasetup/ProducerDemo.java
@@ -0,0 +1,17 @@
+package com.github.ptndoss.kafkasetup;
+
+import org.apache.kafka.clients.producer.Producer;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+
+//        Three Steps in creting Producer
+//        Create Producer Properties
+//        Create Producer
+//        Send Data
+
+
+        System.out.println("Hello From Producer Demo");
+    }
+}
